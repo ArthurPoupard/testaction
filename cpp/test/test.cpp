ee702bcb20c1d9415bafb7be53c83f595c9874e2
ArthurPoupard <168419887+ArthurPoupard@users.noreply.github.com>
2025-04-03 16:38:47 +0200

    Update test.cpp

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -1,6 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-//#include "../function/function.h"
+#include "function.h"
 
 
 int local(int number) {
@@ -11,4 +11,10 @@ TEST_CASE("testing the local function") {
     CHECK(local(1) == 3);
     CHECK(local(0) == 2);
     CHECK(local(-3) == -1);
+}
+
+TEST_CASE("testing the other function") {
+    CHECK(otherfunc(1) == 1);
+    CHECK(otherfunc(0) == 0);
+    CHECK(otherfunc(-3) == -3);
 }
