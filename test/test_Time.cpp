c8e420f8df2e0203021ee2dee5d8082ce3a250c4
ArthurPoupard <168419887+ArthurPoupard@users.noreply.github.com>
2025-04-03 14:07:20 +0200

    Update test_Time.cpp

--- test/test_Time.cpp
+++ test/test_Time.cpp
@@ -1,46 +1,16 @@
+
+#include <github_actions_gtest_example/github_actions_gtest_example.h>
+
 #include <gtest/gtest.h>
 
-#include <Time.hpp>
-
-
-// -------------------- test fixture --------------------
-class TestTimeFixture : public ::testing::Test
-{
-protected:
-    virtual void SetUp()
-    {
-        time_0_sec_ = Time(0, 0);
-        time_1_sec_ = Time(1, 0);
-    }
-
-    //virtual void TestDown() {}
-
-    Time time_0_sec_;
-    Time time_1_sec_;
-};
-
-
-// -------------------- test case --------------------
-TEST_F(TestTimeFixture, typicalWithFixture)
-{
-    // EXPECT_LE() cause link error in GoogleTest v1.10.0
-    EXPECT_FALSE( time_0_sec_ == time_1_sec_ );
-    EXPECT_TRUE( time_0_sec_ != time_1_sec_ );
-    EXPECT_FALSE( time_0_sec_ > time_1_sec_ );
-    EXPECT_TRUE( time_0_sec_ < time_1_sec_ );
-
-    EXPECT_TRUE( time_1_sec_ >  Time(0, 999999) );
-    EXPECT_TRUE( time_1_sec_ == Time(1, 00000) );
-    EXPECT_TRUE( time_1_sec_ <  Time(1, 000001) );
-
-    EXPECT_TRUE( (time_0_sec_ += Time(1, 0)) == time_1_sec_ );
+TEST(githubActionsGtestExampleTest, firstTest) {
+  EXPECT_TRUE( 1 == 1 );
 }
 
-TEST(TestTime, getStr)
-{
-    const std::size_t size = 32;
-    char buffer[size];
-
-    EXPECT_STREQ( "000000000000.999999", Time(0, 999999).getStr(buffer, size) );
-    EXPECT_STREQ( "000000000001.000001", Time(1, 000001).getStr(buffer, size) );
+TEST(githubActionsGtestExampleTest, addTest) {
+  const int a = 1;
+  const int b = 3;
+  const int result = githubActionsGtestExample::add(a, b);
+  
+  EXPECT_EQ( result, a + b );
 }
