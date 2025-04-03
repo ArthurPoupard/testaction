1521cc14954fdbae7770aedc1337de72cc4e5b99
ArthurPoupard <168419887+ArthurPoupard@users.noreply.github.com>
2025-04-03 16:19:56 +0200

    Update test.cpp

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -1,13 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "../function/function.hpp"
+#include "./function/function.hpp"
 
 
 int function(int number) {
     return(number + 2);
 }
 
-TEST_CASE("testing the factorial function") {
+TEST_CASE("testing the local function") {
     CHECK(function(1) == 3);
     CHECK(function(0) == 2);
     CHECK(function(-3) == -1);
