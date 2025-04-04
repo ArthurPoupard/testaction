74012046d55f9b271417b020742adf86913fc18a
ArthurPoupard <168419887+ArthurPoupard@users.noreply.github.com>
2025-04-04 08:42:24 +0200

    Update test_premier.cpp

--- cpp/test/test_premier.cpp
+++ cpp/test/test_premier.cpp
@@ -12,3 +12,9 @@ TEST_CASE("testing the local function") {
     CHECK(local(0) == 2);
     CHECK(local(-3) == -1);
 }
+
+TEST_CASE("testing the local function") {
+    CHECK(add(1, 4) == 5);
+    CHECK(add(0, 2) == 2);
+    CHECK(add(-3, 2) == -1);
+}
